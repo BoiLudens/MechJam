6ec7dff58352e6f5e48190433fd811e80c009028
Ya Boi <mattacoker@gmail.com>
2022-07-27 14:34:24 -0400

    fixed particles

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -15,9 +15,10 @@ func instantiate(start, target):
 func _on_Bullet_body_entered(body):
 	if body.is_in_group('mobs'):
 		var hit_particles = hit_particles_scene.instance()
-		body.queue_free()
-		add_child(hit_particles)
+		get_parent().add_child(hit_particles)
 		hit_particles.transform = transform
+		body.queue_free()
+		queue_free()
 	
 func _on_Timer_timeout():
 	queue_free()
--- Scripts/HitParticles.gd
+++ Scripts/HitParticles.gd
@@ -3,3 +3,5 @@ extends Particles
 
 func _ready():
 	set_as_toplevel(true)
+	emitting = true
+
